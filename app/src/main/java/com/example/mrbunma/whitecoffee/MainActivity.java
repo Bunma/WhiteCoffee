873597e52f9a1a28499d1feb8abf0524decf65ec
Bunma <33587175+Bunma@users.noreply.github.com>
2018-12-09 16:03:27 +0700

    New

--- app/src/main/java/com/example/mrbunma/whitecoffee/MainActivity.java
+++ app/src/main/java/com/example/mrbunma/whitecoffee/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.mrbunma.whitecoffee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageSwitcher imageSwitcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageSwitcher = findViewById(R.id.imageSwitcher);
+        imageSwitcher.setImageResource(R.drawable.ic_launcher_background);
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView myView = new ImageView(getApplicationContext());
+                return myView;
+            }
+        });
     }
 }
